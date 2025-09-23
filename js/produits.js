25f31e054b44fa8631f3886caf796b33e5981bf1
Souraka229 <bagoudou046@gmail.com>
2025-09-23 11:01:51 +0100

    Create js/produits.js

---
+++ js/produits.js
@@ -0,0 +1,34 @@
+const produits = [
+    {
+        id: "ps5-slim",
+        nom: "PS5 Slim Scellée",
+        prix: "390.000 FCFA",
+        images: ["images/produits/ps5-1.jpg", "images/produits/ps5-2.jpg", "images/produits/ps5-3.jpg"],
+        description: "Version récente, design compact, +10 jeux offerts",
+        categorie: "consoles"
+    },
+    {
+        id: "ps4-fat",
+        nom: "PS4 Fat",
+        prix: "165.000 FCFA",
+        images: ["images/produits/ps4-1.jpg", "images/produits/ps4-2.jpg"],
+        description: "Reconditionnée, 500Go, 10-15 jeux, garantie",
+        categorie: "consoles"
+    },
+    {
+        id: "ps3-slim",
+        nom: "PS3 Slim",
+        prix: "85.000 FCFA",
+        images: ["images/produits/ps3-1.jpg"],
+        description: "Reconditionnée, 250Go, jeux inclus",
+        categorie: "consoles"
+    },
+    {
+        id: "ps2",
+        nom: "PS2",
+        prix: "35.000 FCFA",
+        images: ["images/produits/ps2-1.jpg"],
+        description: "Console classique, idéale pour les nostalgiques",
+        categorie: "consoles"
+    }
+];
